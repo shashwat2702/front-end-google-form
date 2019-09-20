b8a9f6e4b3667d06709a2d6ef2ffd37446342082
Shashwat Sinha <shashwat-sinha@VIMID-SinhaS-LM.local>
2019-09-20 22:54:25 +0530

    Boiler Plate setup for scss files and test cases
    * Added Navbar as well

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <nav className="navLink">
+      <ul>
+        <li>Home</li>
+        <li>Create new Form</li>
+        <li>Submit Responses</li>
+        <li>View Responses</li>
+      </ul>
+    </nav>
+  )
+};
+
+export default Navbar;
